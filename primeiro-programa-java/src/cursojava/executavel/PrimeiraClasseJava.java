9f17aa2383aeb542b10821ff934b256f232ed968
natan <natan@DESKTOP-DO4395V>
2023-03-28 17:41:06 -0400

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+
+		
+		
+		System.out.println("Meu primeiro programa em java - Do curso Formação Java Web");
+	}
+
+}
