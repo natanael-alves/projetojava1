30972d298d76ee43492c1ebe0e6507c99e60714f
natan <natan@DESKTOP-DO4395V>
2023-03-28 18:46:31 -0400

    adicionando o campo de idade na classe pessoa.

--- primeiro-programa-java/src/curso/java/natanael/pessoa.java
+++ primeiro-programa-java/src/curso/java/natanael/pessoa.java
@@ -6,6 +6,16 @@ public class pessoa {
 	
 	private String sobrenome;
 
+	private int idade;
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 	public String getNome() {
 		return nome;
 	}
